3fa4b8ea06d96429f698a89b5d003b359b7110c8
AkshayChandra09 <akshay.chandrachood.com>
2018-03-23 21:51:54 -0500

    committed new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
+import { HttpModule } from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ListtasksComponent } from './components/listtasks/listtasks.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskService } from './shared-service/task.service';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProjectService } from './shared-service/project.service';
+import { ReportComponent } from './components/report/report.component';
 
+import { UserService } from './shared-service/user.service';
+
+
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+import * as FintTheme from 'fusioncharts/themes/fusioncharts.theme.fint';
+import { FusionChartsModule } from 'angular4-fusioncharts';
+import { LoginComponent } from './components/login/login.component';
+
+
+FusionChartsModule.fcRoot(FusionCharts, Charts, FintTheme);
+
+const appRoutes:Routes = [
+  {path:'', component:LoginComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'view_tasks', component:ListtasksComponent},
+  {path:'add_task', component:TaskFormComponent},
+  {path:'edit_task/:task_id', component:EditTaskComponent},
+  {path:'statistics', component:ReportComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListtasksComponent,
+    TaskFormComponent,
+    AdminPanelComponent,
+    EditTaskComponent,
+    DashboardComponent,
+    ReportComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FusionChartsModule
   ],
-  providers: [],
+  providers: [TaskService, ProjectService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    id:Number;
+    user_name:string;
+    password:string;
+}
